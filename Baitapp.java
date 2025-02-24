a5cdb04960b55eca660ae26c55db6006337c2fab
nguyen11565 <hoangtuannguyen1511@gmail.com>
2025-02-24 15:31:41 +0700

    Add files via upload

---
+++ Baitapp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baitapp;
+import java.util.Scanner;
+class Baitapp {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+   
+    String ten = "nguyen";
+    String ngaysinh = "15/11";
+    String mssv = "1250080120";
+   
+      System.out.println("Ten: " + ten);
+      
+       System.out.println("ngaysinh: " + ngaysinh);
+        
+       System.out.println("mssv: " + mssv);
+    }
+}
+
+
